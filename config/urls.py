b4e4aee7a894937c9ad27719c67a32865b0206de
asbenjamin <shammahbenjamin@gmail.com>
2022-08-02 18:43:23 +0300

    correct landing page to follow django template style

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls'))
+    path('users', include('users.urls')),
+    path('excel', include('excel.urls'))
 ]
