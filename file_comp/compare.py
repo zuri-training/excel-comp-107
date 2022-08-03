ce602ec989355fefc9b1b653db33f9ce78a59a79
esperanzer <esperanzatm95@gmail.com>
2022-08-03 11:14:34 +0200

    was testing code highlight functionality but dint work

--- file_comp/compare.py
+++ file_comp/compare.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from openpyxl import Workbook
 # pd is just a short name for referencing pandas
 # dftest1/2 means dataframe with the excel name in my folder
 
@@ -7,12 +8,17 @@ dftest1=pd.read_excel('files/test1.xlsx'
 )
 
 dftest2=pd.read_excel('files/test2.xlsx')
+# df=df[df.duplicated(subset=['val1','val2'], keep=False)]
+# dftest1['Duplicate'] = dftest1.duplicated(keep=False).map({True:'Yes', False:'No'})
+# dftest2['Duplicate'] = dftest2.duplicated(keep=False).map({True:'Yes', False:'No'})
+
+
+
 
 # print(dftest1.equals(dftest2))
 
 comparevalues = dftest1.values == dftest2.values
 print(comparevalues)
-
 rows,cols=np.where(comparevalues==True)
 for item in zip(rows,cols):
      dftest1.iloc[item[0], item[1]] = '{} --> {}'.format(dftest1.iloc[item[0], item[1]],dftest2.iloc[item[0], item[1]])
