d8367119534c615742777d550c3118979868613d
David05 <davidgreendevlops@gmail.com>
2022-08-04 20:56:03 +0100

    validation for authentication with forgot password

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users', include('users.urls')),
-    path('excel', include('excel.urls'))
+    path('excel/', include('excel.urls')),
+    path('users/', include('users.urls')),
+    
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,30 @@
 from django.urls import path
-from . import views
 
-app_name = "users"   
+from .views import (
+   LogInView, ResendActivationCodeView, RemindUsernameView, SignUpView, ActivateView, LogOutView,
+    ChangeEmailView, ChangeEmailActivateView, ChangeProfileView, ChangePasswordView,
+    RestorePasswordView, RestorePasswordDoneView, RestorePasswordConfirmView,
+)
 
+app_name = 'users'
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("home", views.homepage, name="homepage"),
-    path("register", views.register_request, name="register"),
-    path("login", views.login_request, name="login"),
-    path("logout", views.logout_request, name= "logout"),
+    path('log-in/', LogInView.as_view(), name='log_in'),
+    path('log-out/', LogOutView.as_view(), name='log_out'),
+
+    path('resend/activation-code/', ResendActivationCodeView.as_view(), name='resend_activation_code'),
+
+    path('sign-up/', SignUpView.as_view(), name='sign_up'),
+    path('activate/<code>/', ActivateView.as_view(), name='activate'),
+
+    path('restore/password/', RestorePasswordView.as_view(), name='restore_password'),
+    path('restore/password/done/', RestorePasswordDoneView.as_view(), name='restore_password_done'),
+    path('restore/<uidb64>/<token>/', RestorePasswordConfirmView.as_view(), name='restore_password_confirm'),
+
+    path('remind/username/', RemindUsernameView.as_view(), name='remind_username'),
+
+    path('change/profile/', ChangeProfileView.as_view(), name='change_profile'),
+    path('change/password/', ChangePasswordView.as_view(), name='change_password'),
+    path('change/email/', ChangeEmailView.as_view(), name='change_email'),
+    path('change/email/<code>/', ChangeEmailActivateView.as_view(), name='change_email_activation'),
 ]
