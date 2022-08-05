cd928471f85ade47ac5ef4ab233a3390a4326bf2
asbenjamin <shammahbenjamin@gmail.com>
2022-08-05 06:01:23 +0300

    add models

--- excel/models.py
+++ excel/models.py
@@ -1,3 +1,23 @@
+from __future__ import unicode_literals
+from django.conf import settings
 from django.db import models
+import uuid
 
-# Create your models here.
+def uuid_default():
+    return uuid.uuid4()
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'documents/'+'user_{0}/{1}/{2}'.format(instance.user.username, instance.uuid, filename)
+
+
+class SessionInstance(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid_default, editable=False)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='session_instances', on_delete=models.CASCADE)
+
+
+class Document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to=user_directory_path)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    session_instance = models.ForeignKey(SessionInstance, on_delete=models.CASCADE, related_name='files')
--- excel/views.py
+++ excel/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .forms import DocumentForm
+from .models import Document
 
 
 def home(request):
     # documents = Document.objects.all()
     return render(request, 'excel/home.html')
+
+def create_to_feed(request):
+    user = request.user
+    if request.method == 'POST':
+        form = DocumentForm(request.POST)
+        file_form = DocumentForm(request.POST, request.FILES)
+        files = request.FILES.getlist('file') #field name in model
+        if form.is_valid() and file_form.is_valid():
+            feed_instance = form.save(commit=False)
+            feed_instance.user = user
+            feed_instance.save()
+            for f in files:
+                file_instance = Document(file=f, feed=feed_instance)
+                file_instance.save()
+    else:
+        form = Document()
+        file_form = Document()
