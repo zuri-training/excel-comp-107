8e73d92fefd2e36230ea257e01805a9c3999a70e
esperanzer <esperanzatm95@gmail.com>
2022-08-08 12:42:26 +0200

    added out file

--- file_comp/compare.py
+++ file_comp/compare.py
@@ -1,21 +1,14 @@
 import pandas as pd
 import numpy as np
 from openpyxl import Workbook
-import datetime
-
-dftest1=pd.read_excel('files/test1.xlsx'
-)
  
-dftest2=pd.read_excel('files/test2.xlsx')
+dftest1 = pd.read_excel('files/test1.xlsx')
+ 
+dftest2 = pd.read_excel('files/test2.xlsx')
 # print(dftest1.equals(dftest2))
 comparevalues = dftest1.values == dftest2.values
 print(comparevalues)
-rows,cols=np.where(comparevalues==True)
-for item in zip(rows,cols):
-     dftest1.iloc[item[0], item[1]] = '{} --> {}'.format(dftest1.iloc[item[0], item[1]],dftest2.iloc[item[0], item[1]])
-     dftest1.to_excel('./files/output.xlsx',index=True,header=False)
-
-
-
-
-
+rows, cols = np.where(comparevalues == True)
+for item in zip(rows, cols):
+    dftest1.iloc[item[0], item[1]] = '{} #0000FF {}'.format(dftest1.iloc[item[0], item[1]], dftest2.iloc[item[0], item[1]])
+    dftest1.to_excel('./files/output.xlsx', index=True, header=False)
