2ed73caca329bd336850e35a1e405f73905d53bd
asbenjamin <shammahbenjamin@gmail.com>
2022-08-10 13:03:08 +0300

    add database conn

--- config/settings.py
+++ config/settings.py
@@ -92,6 +92,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
