05b2c75236e0b65d480a9e7e22fca9200c207b78
asbenjamin <shammahbenjamin@gmail.com>
2022-08-10 11:34:36 +0300

    create config for heroku hosting

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'excelcomp.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'excelcomp.herokuapp.com']
 
 
 # Application definition
--- config/urls.py
+++ config/urls.py
@@ -28,13 +28,21 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.urls import re_path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('excel.urls')),
 
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
