47ea44069fde2a29aaf1d1d5bd6709dc1d9ffb3a
asbenjamin <shammahbenjamin@gmail.com>
2022-08-11 13:43:50 +0300

    add cloudinary storage

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_URL = 'static/'
 
