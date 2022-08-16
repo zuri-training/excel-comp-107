20963a58ed2e8f3fb3c5acf7f6d6639466ecc058
esperanzer <esperanzatm95@gmail.com>
2022-08-16 22:24:39 +0200

    adde code that highlight duplicates

--- file_comp/compare.py
+++ file_comp/compare.py
@@ -1,25 +1,64 @@
 import pandas as pd
-import numpy as np
-from openpyxl import Workbook
-from dotenv import load_dotenv
-from openpyxl.styles import PatternFill
- 
+import xlwings as xw
+
 dftest1 = pd.read_excel('files/test1.xlsx')
- 
 dftest2 = pd.read_excel('files/test2.xlsx')
-# print(dftest1.equals(dftest2))
-comparevalues = dftest1.values == dftest2.values
-print(comparevalues)
-rows, cols = np.where(comparevalues == True)
-for item in zip(rows, cols):
-    dftest1.iloc[item[0], item[1]] = '{}  {}'.format(dftest1.iloc[item[0], item[1]], dftest2.iloc[item[0], item[1]])
-    dftest1.to_excel('./files/output.xlsx', index=True, header=False)
-    
-    # format1 = workbook.add_format({'bg_color':   '#FFC7CE',
-                            #    'font_color': '#9C0006'})
-    def view_result(request):
-        def highlight_cells(val):
-            pat=str(val)
-            color = 'orange' if pat [0]== '*' else 'blue'
-            return 'background-color: {}'.format(color)
-        h = dftest1.style.applymap(highlight_cells)
+
+excl_merged = pd.DataFrame()
+excl_merged = excl_merged.append(
+    dftest1, ignore_index=True)
+excl_merged = excl_merged.append(
+    dftest2, ignore_index=True)
+
+print("Dataset with duplicates")
+print(excl_merged)
+allDuplicates = excl_merged[excl_merged.duplicated(keep='last')]
+duplicateValues = allDuplicates.drop_duplicates(keep='first')
+print("Duplicates from dataset")
+print(duplicateValues)
+noDuplicates = excl_merged.drop_duplicates(keep='first')
+print("Dataset without duplicates")
+print(noDuplicates)
+
+noDuplicates.to_excel('./files/output.xlsx', index=False, header=True)
+path = r"./files/output.xlsx"
+
+with xw.App(visible=False) as app:
+    wb = xw.Book(path)
+    ws = wb.sheets[0]
+
+    for a_cell in ws["A2:A5"].expand("down"):
+        if type(a_cell.value) in [float, int]:
+            if a_cell.value in duplicateValues.values:
+                a_cell.color = (169, 208, 142)
+            elif a_cell.value in duplicateValues.values:
+                a_cell.color = (192, 0, 0)
+    wb.save(path)
+    wb.close()
+
+#noDuplicates.to_excel('./files/output.xlsx', index=False, header=True)
+
+
+#duplicate = excl_merged[excl_merged.duplicated(keep='first')]
+# Print the resultant Dataframe
+#print(duplicate)
+
+# exports the dataframe into excel file with
+# specified name.
+#excl_merged.to_excel('./files/output.xlsx', index=False)
+
+# duplicates = excl_merged.duplicated(keep=False)
+# df = pd.DataFrame()
+# duplicate = df[duplicates]
+#print(duplicate)
+#comparevalues = dftest1.values == dftest2.values
+#print(comparevalues)
+
+#dftest1['diff'] = np.where(dftest1['file_1'] == dftest2['file_2'] , '0', '1')
+
+#comparevalues = dftest1. == dftest2.values
+#print(comparevalues)
+#rows = np.where(comparevalues == True)
+#for item in zip(rows):
+ #   dftest1.iloc[item[0], item[1]] = '{}  {}'.format(dftest1.iloc[item[0], item[1]], dftest2.iloc[item[0], item[1]])
+  #  dftest1.to_excel('./files/output.xlsx', index=True, header=False)
